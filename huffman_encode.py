c4591d5423e8971ae1eca6dee35d66ac2b2a2cbf
Young Seok Tony Kim <mightydeveloper@gmail.com>
2018-09-07 17:24:47 +0900

    Added huffman_encoding script

--- huffman_encode.py
+++ huffman_encode.py
@@ -1,7 +1,7 @@
 import argparse
 import torch
 
-from net.huffmancoding import huffman_encode
+from net.huffmancoding import huffman_encode_model
 import util
 
 parser = argparse.ArgumentParser(description='Huffman encode a quantized model')
@@ -9,10 +9,8 @@ parser.add_argument('model', type=str, help='saved quantized model')
 parser.add_argument('--no-cuda', action='store_true', default=False, help='disables CUDA')
 args = parser.parse_args()
 
-
 use_cuda = not args.no_cuda and torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else 'cpu')
 
-
 model = torch.load(args.model)
-huffman_encode(model)
+huffman_encode_model(model)
